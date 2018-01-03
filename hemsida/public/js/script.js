d2eaddfcd045ec463a67d8f87ebadcbd70007915
itggot-elias-carlson <elias.carlson@itggot.se>
2018-01-03 17:57:00 +0100

    Fixade Jquery JS

--- hemsida/public/js/script.js
+++ hemsida/public/js/script.js
@@ -1,19 +1,11 @@
-// document.querySelector(".menubtn").onclick = function() {
-//     document.querySelector(".sidenav").style.width = "calc(100vw - 60px)";
-// }
-
-// document.querySelector("main", "header", "footer").onclick = function() {
-//     document.querySelector(".sidenav").style.width = "0";
-// }
-
 $(document).ready(function(){
     $(".menubtn").click(function(){
-        $(".sidenav").style.width = "calc(100vw - 60px)";
+        $(".sidenav").css('width', 'calc(100vw - 60px)');
     });
 });
 
 $(document).ready(function(){
-    $(".menubtn").click(function(){
-        $(".sidenav").style.width = "0";
+    $("main,footer").click(function(){
+        $(".sidenav").css('width', '0');
     });
 });
