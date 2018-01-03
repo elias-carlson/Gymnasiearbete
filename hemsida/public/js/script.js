8e64006a1ea38fb88bb535537da880e2abe61d69
kian00sh <kiaplato@gmail.com>
2018-01-03 17:45:10 +0100

    js details fixed

--- hemsida/public/js/script.js
+++ hemsida/public/js/script.js
@@ -1,7 +1,19 @@
-document.querySelector(".menubtn").onclick = function() {
-    document.querySelector(".sidenav").style.width = "calc(100vw - 60px)";
-}
+// document.querySelector(".menubtn").onclick = function() {
+//     document.querySelector(".sidenav").style.width = "calc(100vw - 60px)";
+// }
 
-document.querySelector("main", "header", "footer").onclick = function() {
-    document.querySelector(".sidenav").style.width = "0";
-}
+// document.querySelector("main", "header", "footer").onclick = function() {
+//     document.querySelector(".sidenav").style.width = "0";
+// }
+
+$(document).ready(function(){
+    $(".menubtn").click(function(){
+        $(".sidenav").style.width = "calc(100vw - 60px)";
+    });
+});
+
+$(document).ready(function(){
+    $(".menubtn").click(function(){
+        $(".sidenav").style.width = "0";
+    });
+});
