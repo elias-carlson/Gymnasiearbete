8c618c72ca5a8aa42f2554ddd5d711e67f36c76a
itggot-elias-carlson <elias.carlson@itggot.se>
2018-02-02 17:54:20 +0100

    Control panel

--- hemsida/public/js/script.js
+++ hemsida/public/js/script.js
@@ -12,27 +12,63 @@ $(function(){
         $(".login form").css('display', 'flex');
         $(".loginfield").animate({width: '150px'}, 500);
     });
-});
 
-function showLoginError(){
-    $(".login #loginbtn").css('display', 'none');
-    $(".login form").css('display', 'flex');
-    $(".loginfield").css('width', '150px');
-};
-
-function registerError(element) {
-    $(element).css('border', 'red 1px solid');
-}
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() < 100 || $(this).scrollTop() > 100 ) {
-        $("#toTopBtn").fadeIn();
-    } else {
-        $("#toTopBtn").fadeOut();   
+    function showLoginError(){
+        $(".login #loginbtn").css('display', 'none');
+        $(".login form").css('display', 'flex');
+        $(".loginfield").css('width', '150px');
+    };
+    
+    function registerError(element) {
+        $(element).css('border', 'red 1px solid');
     }
-});
+    
+    if(top.location.pathname == "/app") {
 
-// When the user clicks on the button, scroll to the top of the document
-$("#toTopBtn").click(function() {
-    $("html").animate({scrollTop: '100px'});
+        $(function() {
+            var i = 1;
+            $(".dropMenu").children().each(function() {
+                $(".options").append(`
+                    <div id="opt_`+i+`">
+                        <div class="style">
+                            <div class="bg">
+                                <p>Background </p>
+                                <i class="material-icons">arrow_drop_down</i>
+                                <p>Color:</p>
+                                <input class="jscolor" value="ab2567">
+                                <p>Image:</p>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="content">
+
+                        </div>
+                    </div>`
+                );
+                i++;
+            });
+
+        });
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() < 100 || $(this).scrollTop() > 100 ) {
+                $("#toTopBtn").fadeIn();
+            } else {
+                $("#toTopBtn").fadeOut();   
+            }
+        });
+        
+        $("#toTopBtn").click(function() {
+            $("html").animate({scrollTop: '100px'});
+        });
+    
+        $(".dropBtn").click(function() {
+            $(".dropMenu").slideToggle();
+        });
+    
+        $(".dropMenu").children().click(function() {
+            var opt_id = $(this).attr("id");
+            $("."+opt_id).toggleClass(selected);
+        });
+    } 
 });
