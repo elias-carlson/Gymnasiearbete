58966cacdb9c01bf9e46508418e02d07c76a4d28
itggot-elias-carlson <elias.carlson@itggot.se>
2018-02-04 15:31:20 +0100

    Screenshots + home paged fixed

---
+++ templates/js/script.js
@@ -0,0 +1,15 @@
+var iframe_find = $('iframe').contents()
+
+
+$(document).ready(()=> {
+  iframe_find.find('section').not('section.first').hide();
+  iframe_find.find('#menu').children().click(function() {
+    var page_id = $(this).attr('id');
+    if (!$(this).hasClass('selected')) {
+      iframe_find.find('#menu').find('.selected').toggleClass('selected');
+      $(this).toggleClass('selected');
+      iframe_find.find('section').hide();
+      iframe_find.find('section.' + page_id).show();
+    };
+  });
+});
