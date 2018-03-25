2b37a65b3adceec88cb664654cc0d7ca79a1f89d
kian00sh <kiaplato@gmail.com>
2018-03-25 18:36:32 +0200

    please work for the love of god

--- hemsida/public/template/my_simple_company/js/script.js
+++ hemsida/public/template/my_simple_company/js/script.js
@@ -3,10 +3,10 @@ var iframe_find = $('iframe').contents()
 
 $(document).ready(()=> {
   iframe_find.find('section').not('section.home').hide();
-  iframe_find.find('#menu').children().click(function() {
+  iframe_find.find('.navigation_menu').children().click(function() {
     var page_id = $(this).attr('id');
     if (!$(this).hasClass('selected')) {
-      iframe_find.find('#menu').find('.selected').toggleClass('selected');
+      iframe_find.find('.navigation_menu').find('.selected').toggleClass('selected');
       $(this).toggleClass('selected');
       iframe_find.find('section').hide();
       iframe_find.find('section.' + page_id).show();
